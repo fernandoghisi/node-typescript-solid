999ac1734754cca8d50e6e8d24ff3c80c4f28bc0
Fernando Ghisi <fernandoghisi@gmail.com>
2021-07-28 16:19:26 -0300

    externalizing dom manipulation

--- src/ShareButton.ts
+++ src/ShareButton.ts
@@ -1,24 +1,24 @@
+import EventHandler from "./EventHandler";
 export default class ShareButton {
+  eventHandler: EventHandler;
   url: string;
 
   constructor(url: string) {
     this.url = url;
+    this.eventHandler = new EventHandler();
   }
 
   bind(clazz: string, socialNetwork: string) {
     let link: string;
-    if(socialNetwork === "twitter") {
+    if (socialNetwork === "twitter") {
       link = `https://twitter.com/share?url=${this.url}`;
     }
-    if(socialNetwork === "facebook") {
+    if (socialNetwork === "facebook") {
       link = `https://facebook.com/sharer.php?u=${this.url}`;
     }
-    if(socialNetwork === "linkedin") {
+    if (socialNetwork === "linkedin") {
       link = `https://linkedin.com/shareArticle?url=${this.url}`;
     }
-    const elements: any = document.querySelectorAll(clazz);
-    for (const element of elements) {
-      element.addEventListener("click", () => window.open(link));
-    }
+    this.eventHandler.addEventListenerToClass(clazz, "click", () => window.open(link));
   }
 }
