f507ea508cdcd8886bc0be493b6e913772470705
Fernando Ghisi <fernandoghisi@gmail.com>
2021-08-16 18:49:46 -0300

    applying dependency inversion solid principle

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,16 @@ import ShareButtonTwitter from "./ShareButtonTwitter";
 import ShareButtonFacebook from "./ShareButtonFacebook";
 import ShareButtonLinkedin from "./ShareButtonLinkedin";
 import ShareButtonPrint from "./ShareButtonPrint";
+import AbstractShareButton from "./AbstractShareButton";
+import DOMEventHandler from "./DOMEventHandler";
 
-const twitter = new ShareButtonTwitter(".btn-twitter","https://www.typescriptlang.org/");
+const eventHandler = new DOMEventHandler();
+
+const twitter: AbstractShareButton = new ShareButtonTwitter(eventHandler, ".btn-twitter", "https://www.typescriptlang.org/");
 twitter.bind();
-const facebook = new ShareButtonFacebook(".btn-facebook","https://www.typescriptlang.org/");
+const facebook: AbstractShareButton = new ShareButtonFacebook(eventHandler, ".btn-facebook", "https://www.typescriptlang.org/");
 facebook.bind();
-const linkedin = new ShareButtonLinkedin(".btn-linkedin","https://www.typescriptlang.org/");
+const linkedin: AbstractShareButton = new ShareButtonLinkedin(eventHandler, ".btn-linkedin", "https://www.typescriptlang.org/");
 linkedin.bind();
-const print = new ShareButtonPrint(".btn-print");
+const print: AbstractShareButton = new ShareButtonPrint(eventHandler, ".btn-print");
 print.bind();
